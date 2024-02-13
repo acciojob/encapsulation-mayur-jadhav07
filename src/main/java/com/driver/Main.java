8b357c68e94e11146ee9ef35f268a3567d277569
mayur-jadhav07 <jmayur309@gmail.com>
2024-02-13 09:23:30 +0530

    Implemented all tasks

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly rwOnlyObj = new RWOnly();
+
+        // Uncommenting the following line will result in a compilation error
+        // rwOnlyObj.name = "Mayur"; // Error: The field RWOnly.name is not visible
+
+        // Uncommenting the following line will result in a compilation error
+        // System.out.println(rwOnlyObj.name); // Error: The field RWOnly.name is not visible
+
+        // Set value using setter
+        rwOnlyObj.setName("Mayur Jadhav");
+
+        // Access value using getter
+        System.out.println("Name: " + rwOnlyObj.getName());
+    }
 }
